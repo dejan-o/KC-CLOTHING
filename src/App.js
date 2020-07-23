793bee74d00e32e75f362077ab3dd73847e7f0d3
unknown <dejan.obr@hotmail.com>
2020-07-23 06:07:17 +0200

    inserting react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
+import {Switch, Route} from 'react-router-dom'
+
+//JUST FOR TESTING
+const Hats = () =>{
+  return <h1>HATS</h1>
+}
+
+
+
 
 function App() {
   return (
-    <Homepage />
+    <div>
+        <Switch>
+            <Route exact path='/' component={Homepage}/>
+            <Route path='/hats' component={Hats} />
+        </Switch>
+    </div>
   );
 }
 
